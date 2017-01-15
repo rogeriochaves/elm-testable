d7a9f53970cc153ad76775c8472006211b152023
Rogerio Chaves <rogeriocfj+github@gmail.com>
2017-01-15 11:28:54 -0200

    Add original files to generate diff

---
+++ examples/originals/generate-diff.sh
@@ -0,0 +1,3 @@
+git diff HEAD:./RandomGif.elm ../RandomGif.elm > RandomGif.diff
+git diff HEAD:./Spelling.elm ../Spelling.elm > Spelling.diff
+git diff HEAD:./WebSockets.elm ../WebSockets.elm > WebSockets.diff
