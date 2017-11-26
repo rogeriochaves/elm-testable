77585ce9e5b6e02d1a72ae13c39b85add066d8f2
Rogerio Chaves <rogeriocfj@gmail.com>
2017-11-26 09:45:43 -0300

    Some test fixies and change repo name back

--- examples/tests/RandomGifTests.elm
+++ examples/tests/RandomGifTests.elm
@@ -3,9 +3,9 @@ module RandomGifTests exposing (..)
 import Expect
 import RandomGif
 import Test exposing (..)
-import Testable.TestContext exposing (..)
-import Testable.Http as Http
 import Testable.Html.Selectors exposing (..)
+import Testable.Http as Http
+import Testable.TestContext exposing (..)
 
 
 catsComponent : Component RandomGif.Msg RandomGif.Model
@@ -26,38 +26,38 @@ assertShownImage expectedImageUrl testContext =
 all : Test
 all =
     describe "RandomGif"
-        [ test "sets initial topic"
-            <| \() ->
+        [ test "sets initial topic" <|
+            \() ->
                 catsComponent
                     |> startForTest
                     |> find [ tag "h2" ]
                     |> assertText (Expect.equal "cats")
-        , test "sets initial loading image"
-            <| \() ->
+        , test "sets initial loading image" <|
+            \() ->
                 catsComponent
                     |> startForTest
                     |> assertShownImage "waiting.gif"
-        , test "makes initial API request"
-            <| \() ->
+        , test "makes initial API request" <|
+            \() ->
                 catsComponent
                     |> startForTest
                     |> assertHttpRequest (Http.getRequest "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=cats")
-        , test "shows the new image on API success"
-            <| \() ->
+        , test "shows the new image on API success" <|
+            \() ->
                 catsComponent
                     |> startForTest
                     |> resolveHttpRequest (Http.getRequest "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=cats")
                         (Http.ok """{"data":{"image_url":"http://giphy.com/cat2000.gif"}}""")
                     |> assertShownImage "http://giphy.com/cat2000.gif"
-        , test "shows the loading image on API failure"
-            <| \() ->
+        , test "shows the loading image on API failure" <|
+            \() ->
                 catsComponent
                     |> startForTest
                     |> resolveHttpRequest (Http.getRequest "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=cats")
-                        (Http.serverError)
+                        Http.serverError
                     |> assertShownImage "waiting.gif"
-        , test "pressing the button makes a new API request"
-            <| \() ->
+        , test "pressing the button makes a new API request" <|
+            \() ->
                 catsComponent
                     |> startForTest
                     |> resolveHttpRequest (Http.getRequest "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=cats")
